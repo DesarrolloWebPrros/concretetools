db7154525d8b3fc6f1381a4b6e6cf9705b6872ab
Osva Lopez <zorro.oscuro87@gmail.com>
2022-03-17 22:27:11 -0400

    promociones2

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,10 +2,17 @@
 
 let express = require('express');
 let ProductoController = require('../controllers/producto');
+let PromocionController = require('../controllers/promocion');
 let api = express.Router();
 
 api.get('/productos', ProductoController.index);
 api.get('/producto/:id', ProductoController.show);
 api.post('/producto', ProductoController.save);
 api.put('/productos/:id', ProductoController.update);
+
+api.get('/promociones', PromocionController.index);
+api.get('/producto/:id', PromocionController.show);
+api.post('/producto', PromocionController.save);
+api.put('/productos/:id', PromocionController.update);
+
 module.exports = api;
