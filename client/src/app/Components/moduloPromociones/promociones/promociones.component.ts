3e9087cd7d4c12852776358c36c73f5e630bc3c7
Osva Lopez <zorro.oscuro87@gmail.com>
2022-03-21 12:40:43 -0600

    Promos component

--- client/src/app/Components/moduloPromociones/promociones/promociones.component.ts
+++ client/src/app/Components/moduloPromociones/promociones/promociones.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-promociones',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromocionesComponent implements OnInit {
 
-  constructor() { }
+  Promociones: any;
+
+  constructor(private Rest: RestService) { }
 
   ngOnInit(): void {
+    this.getPromociones();
+  }
+
+  getPromociones = () => {
+    this.Rest.get(`http://localhost:3768/api/promociones`).subscribe((respuesta:any) => {
+      this.Promociones = respuesta.Promociones;
+      console.log(`Promociones =>>>`,respuesta);
+    });
+    
   }
 
 }
+
